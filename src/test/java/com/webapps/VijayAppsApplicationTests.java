add3560d5c5d9762de5a6447e77ec26909da7771
0031 <vijayakumar.murugan@aalamsoft.com>
2020-03-19 18:47:49 +0530

    Added my project

---
+++ src/test/java/com/webapps/VijayAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VijayAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
